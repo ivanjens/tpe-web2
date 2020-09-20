86f829faafedce99a25b481735427db4cdc18afd
Iván Jensen <ivanerikj@gmail.com>
2020-09-20 11:13:24 -0300

    Añadido ruteo y reglas apache para URL semantica

---
+++ router.php
@@ -0,0 +1,34 @@
+<?php
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+// lee la acción
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'home'; // acción por defecto si no envían
+}
+
+// parsea la accion Ej: suma/1/2 --> ['suma', 1, 2]
+$params = explode('/', $action);
+
+// determina que camino seguir según la acción
+switch ($params[0]) {
+    case 'home':
+        $controller = new TaskController();
+        $controller->showTasks();
+        break;
+    case 'home':
+        $controller = new TaskController();
+        $controller->showTasks();
+        $categoria = $params[1];
+        break;
+    case 'detalles': // ver individualmente un libro
+        $controller = new TaskController();
+        $controller->showTasks();
+        break;
+    default:
+        header("HTTP/1.0 404 Not Found");
+        echo('404 Page not found');
+        break;
+}
