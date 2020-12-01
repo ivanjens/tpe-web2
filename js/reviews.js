fca549d947a82f9ac6461b91fb23adfcd4561e5a
Ivan Jensen <ivanerikj@gmail.com>
2020-12-01 11:13:57 -0300

    Mejoras visuales en las reseñas

--- js/reviews.js
+++ js/reviews.js
@@ -4,17 +4,15 @@ const app = new Vue({
     el: "#app",
     data: {
         reviews: [], // esto es como un assign de smarty
-        error: null,
+        error: 'Aún no hay reseñas para este libro',
     }, 
     methods: {
         deleteReview: async function(reviewId){
-            
                 const reviews = await fetch(`api/reseñas/${reviewId}`, {
                     "method": "DELETE"
                 });
 
                 loadReviews();
-                
         }
       }
 });
@@ -41,9 +39,7 @@ function initPage(){
 
 
     async function addReview() {
-        let url = window.location.href; // obtiene la url
-        let params = url.split('/'); // convierte la url en un arreglo, separando cada palabra con espacios donde hay '/'
-        let book_id = params[params.length-1]; // toma el último item del array donde está la id del libro
+        const book_id = window.location.pathname.substr(window.location.pathname.lastIndexOf('/')+1);
 
     // armo la review
         const reseña = {
@@ -75,9 +71,8 @@ function initPage(){
 }
     // carga todas las reseñas de un libro
     async function loadReviews(){
-        let url = window.location.href; // obtiene la url
-        let params = url.split('/'); // convierte la url en un arreglo, separando cada palabra con espacios donde hay '/'
-        let book_id = params[params.length-1]; // toma el último item del array donde está la id del libro
+
+        const book_id = window.location.pathname.substr(window.location.pathname.lastIndexOf('/')+1);
         
         try{
             const response = await fetch(`api/reseñas/${book_id}`, {
@@ -87,8 +82,6 @@ function initPage(){
                 const reviews = await response.json();
                 console.log(reviews);
                 app.reviews = reviews;
-            } else{
-                app.error = 'Aún no hay reseñas para este libro';
             }
         }
         catch(e){
--- templates/vue/reviews.vue
+++ templates/vue/reviews.vue
@@ -1,18 +1,18 @@
 {literal}
 <section id="app">
     <div v-if="reviews[0]">
-        <div class='border border-light shadow p-3 mb-5 bg-white rounded py-4'>
+        <div class='border border-dark shadow p-3 mb-5 rounded py-4'>
             {/literal}
             {if $user_review == true}
-                <div class="alert alert-warning col-6 offset-3" role="alert">
-                    <p class='text-uppercase text-center'>Ya has escrito tu reseña en este libro</p>
+                <div class="alert alert-info col-6 offset-3" role="alert">
+                    <p class='text-uppercase text-center mt-3'>Ya has escrito tu reseña en este libro</p>
                 </div>
             {/if}
             {literal}
             <div v-for="review in reviews" class="ml-3 mr-3 card text-center mb-5 mt-5 border border-dark">
-                <div class="card-header py-0">
+                <div class="card-header py-0 alert-info text-dark">
                     <p class='float-left mt-3'>Reseña de <span class='font-weight-bold'>{{ review.nombre_usuario }}</span> <span class='ml-2'>{{ review.valoracion }} <i class="fas fa-star punctuation-star"></i></span></p>
-                    <p class='float-right mt-3'>{{ review.fecha }}</p>
+                    <p class='float-right mt-3 font-weight-bold'>{{ review.fecha }}</p>
                 </div>
                 <div class="card-body">
                     
@@ -21,7 +21,7 @@
                     {/literal}
                     {if isset($smarty.session.ADMIN) && $smarty.session.ADMIN == true}
                     <div class='mt-1 mr-2 mb-2 text-right'>
-                        <button v-on:click="deleteReview(review.id)"><img src='https://icongr.am/octicons/shield-x.svg?size=30&color=c92626'/></button>
+                        <button class='btn' v-on:click="deleteReview(review.id)"><img src='https://icongr.am/octicons/shield-x.svg?size=30&color=c92626'/></button>
                     </div>
                     {/if}
                     {literal}
